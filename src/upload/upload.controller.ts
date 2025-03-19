f3a60aa6744fa64af0a48efcef5c535642d2d437
Joahan3250 <joahan.sanchez@alogasoftwaresolution.com>
2025-03-19 09:19:47 -0600

    Cambios para hacer deploy

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('upload')
+export class UploadController {
+    @Post()
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './uploads', // Carpeta donde se guardarán las imágenes
+            filename: (req, file, callback) => {
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+                callback(null, file.fieldname + '-' + uniqueSuffix + extname(file.originalname));
+            },
+        }),
+    }))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        return {
+            filename: file.filename,
+            imageUrl: `https://tu-backend.onrender.com/uploads/${file.filename}`/*la url del backend*/
+        };
+    }
+}
